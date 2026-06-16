96d2bbfaa0b1f33be3b4009019f7005936c9968f
lohasanovich-elam <lohasanovich25@elam.co.uk>
2026-06-16 15:38:46 +0100

    Bullets + Movement
    Coded in

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,37 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody rb;
-    void Start()
+    InputAction move;
+    InputAction shoot;
+    private Rigidbody2D rb;
+
+    public GameObject bulletPrefab;
+    private float cooldownBullet = 1f;
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
+        move = InputSystem.actions.FindAction("Move");
+        shoot = InputSystem.actions.FindAction("Fire");
     }
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 strafeMovement = transform.right * moveHorizontal * 1 * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + strafeMovement);
+        var moveInput = move.ReadValue<Vector2>();
+        rb.linearVelocityX = (moveInput.x*2f);
+        if (shoot.triggered) 
+        {
+            if (cooldownBullet >= 1f)
+            {
+                cooldownBullet = 0f;
+                Instantiate(bulletPrefab, new Vector2(transform.position.x, transform.position.y + 1f), Quaternion.identity);
+            }
+        }
+        if (cooldownBullet < 1f)
+        {
+            cooldownBullet += 0.01f;
+        }
     }
 }
---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    private float speedbullet = 0.01f;
+    private BoxCollider2D boxcollider;
+    private SpriteRenderer spriteRenderer;
+    private Color color;
+
+    private void Awake()
+    {
+        boxcollider = GetComponent<BoxCollider2D>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //color = new Color(Random.Range(0.3f, 1f), Random.Range(0.3f, 1f), Random.Range(0.3f, 1f));
+        //spriteRenderer.color = color;
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.up * speedbullet, Space.World);
+    }
+}
