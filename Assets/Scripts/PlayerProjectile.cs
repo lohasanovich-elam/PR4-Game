1e101d747d4af6444a510b5a79858f994b90876a
lohasanovich-elam <lohasanovich25@elam.co.uk>
2026-06-18 12:43:46 +0100

    Borders Work
    They Work

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
@@ -7,15 +8,28 @@ public class PlayerProjectile : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Color color;
 
+    private bool rainbowbullets = false;
+
     private void Awake()
     {
         boxcollider = GetComponent<BoxCollider2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        color = new Color(Random.Range(0.3f, 1f), Random.Range(0.3f, 1f), Random.Range(0.3f, 1f));
-        spriteRenderer.color = color;
+        if (rainbowbullets == true)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            color = new Color(Random.Range(0.3f, 1f), Random.Range(0.3f, 1f), Random.Range(0.3f, 1f));
+            spriteRenderer.color = color;
+        }
     }
     void Update()
     {
         transform.Translate(Vector3.up * speedbullet, Space.World);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Boundary")
+        {
+            Destroy(gameObject);
+        }
     }
 }
