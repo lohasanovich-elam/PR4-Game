cfb9795cf0065028ec8dd42116e2b75e7e4e5a10
lohasanovich-elam <lohasanovich25@elam.co.uk>
2026-06-29 12:45:12 +0100

    AnimationScript

--- Assets/Scripts/EnemyAnim.cs
+++ Assets/Scripts/EnemyAnim.cs
@@ -6,7 +6,12 @@ public class EnemyAnim : MonoBehaviour
     public Sprite image1;
     public Sprite image2;
     public float waitTime = 0.5f;
+    private SpriteRenderer spriteR;
 
+    private void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         StartCoroutine(Yoghurt());
@@ -14,7 +19,9 @@ public class EnemyAnim : MonoBehaviour
 
     IEnumerator Yoghurt()
     {
+        spriteR.sprite = image1;
         yield return new WaitForSeconds(waitTime);
+        spriteR.sprite = image2;
         yield return new WaitForSeconds(waitTime);
     }
 }
